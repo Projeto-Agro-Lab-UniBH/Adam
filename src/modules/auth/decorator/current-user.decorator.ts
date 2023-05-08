2665f8d8c77f1a9f75bf622efa4e69481f259383
Marlon <Marlonnlmx@outlook.com>
2023-05-08 19:40:11 -0300

    Deleting decorator

--- src/modules/auth/decorator/current-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { AuthRequest } from 'src/modules/auth/models/AuthRequest';
-
-export const CurrentUser = createParamDecorator(
-  (data: unknown, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest<AuthRequest>();
-    return request.user;
-  },
-);
