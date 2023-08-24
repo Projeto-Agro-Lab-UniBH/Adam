ba29b0b6ef3ec28a5b51a4d0b8f16a3427bb8bc1
Marlon <Marlonnlmx@outlook.com>
2023-08-23 23:56:11 -0300

    style: alteração na estrutura de tipos da classe UpdateUserDto para extender tipos parciais

--- src/modules/user/dtos/update-user.dto.ts
+++ src/modules/user/dtos/update-user.dto.ts
@@ -1,44 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsString,
-  IsEmail,
-  MinLength,
-  Matches,
-  IsNotEmpty,
-} from 'class-validator';
+import { PartialType } from '@nestjs/swagger';
+import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto {
-  @ApiProperty()
-  @IsString({ message: 'profile_photo field must be string' })
-  profile_photo?: string;
-
-  @ApiProperty({
-    type: String,
-    description: 'user name',
-    default: 'Theo Montesquieu Hernandes',
-  })
-  @IsNotEmpty({ message: 'username cannot be empty' })
-  @IsString({ message: 'username field must be string' })
-  username?: string;
-
-  @ApiProperty({
-    type: String,
-    description: 'user email',
-    default: 'theo10montesquieu@outlook.com',
-  })
-  @IsString({ message: 'email field must be string' })
-  @IsEmail({}, { message: 'email shoud be valid' })
-  email?: string;
-
-  @ApiProperty({
-    type: String,
-    description: 'password of account',
-    default: '95jhti0-ydtKh9gcn87ybewd#&',
-  })
-  @MinLength(6, { message: 'password must contain at least 8 characters' })
-  @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm, {
-    message: 'The password does not meet the requirements',
-  })
-  @IsString({ message: 'password field must be string' })
-  password?: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
